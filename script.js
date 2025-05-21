1eebbc4c798c8a5840ad24ae59af086970d85b36
cheskyb <cheskybrown@gmail.com>
2025-05-21 20:19:06 +0000

    NOD WHOLESALE

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("emailForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  // This would check against allowed emails in the future
+  alert("Thanks! You'll be redirected soon.");
+});
